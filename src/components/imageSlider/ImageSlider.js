8215d1e37a6a39ba405c02f20481d15810d7beba
kunal jaykam <kunaljaykam@icloud.com>
2021-10-31 11:43:17 +0530

    make it better

--- src/components/imageSlider/ImageSlider.js
+++ src/components/imageSlider/ImageSlider.js
@@ -1,12 +1,53 @@
-// import React from "react";
-// import ImageGallery from "react-image-gallery";
-// import "./ImageSlider.scss";
+
+import React, {Component} from "react";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import {Carousel} from "react-responsive-carousel";
 import {imageSlider} from "../../DrChuck";
+import './ImageSlider.scss';
+
+
+
+export class ImageSlider extends Component {
+  render() {
+    return (
+      <div className="image-slider-container">
+        <Carousel autoPlay={true} infiniteLoop showThumbs={false}>
+          <div>
+            <img src={imageSlider.tsugi} alt="Charles Severance" />
+          </div>
+          <div>
+            <img src={imageSlider.learner} alt="Charles Severance" />
+          </div>
+          <div>
+            <img src={imageSlider.sakai} alt="Charles Severance" />
+          </div>
+        </Carousel>
+      </div>
+    );
+  }
+}
+
+export default ImageSlider;
+
+
+
+
+
+
+
+
+
+
+
 
 // // for testing purposes
+
+
+// import React from "react";
+// import ImageGallery from "react-image-gallery";
+// import "./ImageSlider.scss";
+
 
 // // const image = [
 // //   {
@@ -39,26 +80,3 @@ import {imageSlider} from "../../DrChuck";
 // export default ImageSlider;
 
 
-import React, { Component } from 'react'
-
-export class ImageSlider extends Component {
-  render() {
-    return (
-      <div>
-        <Carousel autoPlay={true} infiniteLoop showThumbs={false}>
-          <div>
-            <img src={imageSlider.tsugi} alt="Charles Severance" />
-          </div>
-          <div>
-            <img src={imageSlider.learner} alt="Charles Severance" />
-          </div>
-          <div>
-            <img src={imageSlider.sakai} alt="Charles Severance" />
-          </div>
-        </Carousel>
-      </div>
-    );
-  }
-}
-
-export default ImageSlider
